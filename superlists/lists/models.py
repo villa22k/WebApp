8c481613f38cb1d7ff00eb627e2db3b1413e2c32
Demo User <demouser@n195-242.mtholyoke.edu>
2016-01-07 14:55:49 -0500

    Create new URLs for lists and added an ID to each item

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
+# Python is running this in order so List comes before 
+class List(models.Model):
+    pass
+
 class Item(models.Model):
     text = models.TextField(default='')
+    # list is a property
+    list= models.ForeignKey(List, default= None)
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request):
     return render(request, 'home.html')
@@ -7,9 +7,14 @@ def home_page(request):
     return render(request, 'home.html')
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
-    return redirect('/lists/the-only-list/')
+    new_list= List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list= new_list)
+    return redirect('/lists/%d/' %(new_list.id,))
 
-def view_list(request):
-    items = Item.objects.all()
+def view_list(request, list_id):
+    # list all the items from that list
+    list_= List.objects.get(id= list_id)
+    items = Item.objects.filter(list=list_)
     return render(request, 'list.html', { 'items': items, })
+
+#list_id will be passed as a capture group -- it will be passed as list_id
