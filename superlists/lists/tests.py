f2fffb321cdc0e9240e70217cfcbdd7fbf17e8f6
Demo User <demouser@n195-242.mtholyoke.edu>
2016-01-08 11:43:59 -0500

    The entire thing works

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -64,6 +64,10 @@ class listViewTest(TestCase):
         self.assertNotContains(response, 'other list item 1')
         self.assertNotContains(response, 'other list item 2')
 
+    def test_passes_correct_list_to_template(self):
+        correct_list= List.objects.create()
+        response= self.client.get('/lists/%d/'% (correct_list.id,))
+        self.assertEqual(response.context['list'], correct_list)
 
 class ItemAndListModelsTest(TestCase):
 
@@ -93,3 +97,29 @@ class ItemAndListModelsTest(TestCase):
         self.assertEqual(first_saved_item.list, new_list)
         self.assertEqual(second_saved_item.text,'Item the second')
         self.assertEqual(second_saved_item.list, new_list)
+
+class NewItemTest(TestCase):
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        correct_list= List.objects.create()
+
+        self.client.post(
+            '/lists/%d/add_item'% (correct_list.id,),
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(),1)
+        new_item= Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        correct_list= List.objects.create()
+
+        response= self.client.post(
+            '/lists/%d/add_item'% (correct_list.id,),
+            data={'item_text': 'A new item for an existing list'}
+
+        )
+
+        self.assertRedirects(response, '/lists/%d/'% (correct_list.id))
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -4,8 +4,6 @@ from lists.models import Item, List
 def home_page(request):
     return render(request, 'home.html')
 
-    return render(request, 'home.html')
-
 def new_list(request):
     new_list= List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list= new_list)
@@ -14,7 +12,15 @@ def new_list(request):
 def view_list(request, list_id):
     # list all the items from that list
     list_= List.objects.get(id= list_id)
+    # this is a list of type Item
     items = Item.objects.filter(list=list_)
-    return render(request, 'list.html', { 'items': items, })
+    return render(request, 'list.html',
+                    { 'items': items, 'list': list_ }
+        )
 
 #list_id will be passed as a capture group -- it will be passed as list_id
+
+def add_item(request, list_id):
+    list_= List.objects.get(id= list_id)
+    Item.objects.create(text=request.POST['item_text'], list= list_)
+    return redirect('/lists/%d/' %(list_.id))
