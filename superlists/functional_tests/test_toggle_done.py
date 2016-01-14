229a71df9b27366e67b7c1ebab49caf237dd8635
Demo User <demouser@n195-242.mtholyoke.edu>
2016-01-14 13:12:19 -0500

    Can mark items done.

--- superlists/functional_tests/test_toggle_done.py
+++ superlists/functional_tests/test_toggle_done.py
@@ -18,6 +18,13 @@ class ToggleDoneTest(TodoFunctionalTest):
         except NoSuchElementException:
             self.fail('%s not marked done' %(todo_text))
 
+    def check_not_marked_off(self, todo_text):
+        try:
+            self.check_marked_off(todo_text)
+        except:
+            return
+        self.fail(''%s' is marked done' %(todo_text))
+
     def test_can_toggle_finished_items(self):
 
         # Edith makes a quick shopping list
@@ -33,7 +40,7 @@ class ToggleDoneTest(TodoFunctionalTest):
         # At the store, Edith puts the feathers in her cart
         # and marks them done on the todo List
         self.toggle_todo_done(['Buy peacock feathers',
-        'Buy fishing line'])
+            'Buy fishing line'])
 
 
         #Edith returns home, re-opens her todo lists
@@ -55,3 +62,34 @@ class ToggleDoneTest(TodoFunctionalTest):
 
         self.toggle_todo_done(['Tie some flys'])
         self.check_marked_off('Tie some flys')
+
+    def test_can_toggle_finished_items(self):
+        # Edith's ties flying is booming, and she wants
+        # a list that she can use over and over
+        self.browser.get(self.live_server_url)
+        self.enter_a_new_item('Buy feathers')
+        self.enter_a_new_item('Buy fishing line')
+        self.enter_a_new_item('Buy sparkles')
+
+        # She looks in her closet and already has fishing line
+        self.toggle_done(['Buy fishing line'])
+        self.mark_item_done('Buy fishing line')
+
+        # She goes to the store, and finishes shopping
+        self.toggle_todo_done([
+            'Buy feathers',
+            'Buy sparkles'
+        ])
+        self.check_marked_off('Buy feathers')
+        self.check_marked_off('Buy fishing line')
+        self.check_marked_off('Buy sparkles')
+
+        # She makes some flys and her closet is empty
+        self.toggle_todo_done([
+            'Buy feathers',
+            'Buy fishing line',
+            'Buy sparkles'
+        ])
+        self.check_not_marked_off('Buy feathers')
+        self.check_not_marked_off('Buy fishing line')
+        self.check_not_marked_off('Buy sparkles')
--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -174,3 +174,31 @@ class ListViewTest(TestCase):
         item2= Item.objects.get(id= item2.id)
         self.assertFalse(item1.is_done)
         self.assertFalse(item2.is_done)
+
+    def test_POST_items_toggles_done(self):
+        current_list= List.objects.create()
+        item1=Item.objects.create(
+            text='Item 1',
+            list= current_list,
+            is_done=True
+            )
+
+        item2=Item.objects.create(
+            text='Item 2',
+            list= current_list,
+            is_done=False
+            )
+
+        # Post data
+        response= self.client.post(
+            '/lists/%d/items/'%(current_list.id,),
+            data ={'mark_item_done': item2.id },
+        )
+
+        self.assertRedirects(response,'/lists/%d/items/'%(current_list.id,))
+
+        # Check item is updated
+        item1= Item.objects.get(id= item1.id)
+        item2= Item.objects.get(id= item2.id)
+        self.assertFalse(item1.is_done)
+        self.assertTrue(item2.is_done)
